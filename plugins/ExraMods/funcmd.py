d8ec36d067d56748e661c8c722ddedb9c2ceb5ce
Alatheesh <100457240+Alatheesh@users.noreply.github.com>
2023-06-07 23:42:29 +0530

    Update funcmd.py
    Signed-off-by: Alatheesh <100457240+Alatheesh@users.noreply.github.com>

--- plugins/ExraMods/funcmd.py
+++ plugins/ExraMods/funcmd.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command("funcmd")) 
 async def funcmd(_, message)
-    hm = await message.reply_text(GETTING ALL MY FUN COMMANDS READY................)
+    hm = await message.reply_text("GETTING ALL MY FUN COMMANDS READY................")
     await hm.edit("/iloveu - TO PROPOSE THE BOT.\n\n/suchitha - TO KNOW ABOUT THE BOT. \n\n/botmood - TO KNOW THE BOT MOOD. ")
 
    

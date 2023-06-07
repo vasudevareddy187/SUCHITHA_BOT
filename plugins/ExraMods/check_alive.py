4e08f371147917d1ef2e1d9e3b9fc5027dfce6e1
Alatheesh <100457240+Alatheesh@users.noreply.github.com>
2023-06-08 00:41:34 +0530

    Update check_alive.py
    Signed-off-by: Alatheesh <100457240+Alatheesh@users.noreply.github.com>

--- plugins/ExraMods/check_alive.py
+++ plugins/ExraMods/check_alive.py
@@ -12,7 +12,7 @@ async def check_alive(_, message):
 @Client.on_message(filters.command("speed", CMD))
 async def ping(_, message):
     start_t = time.time()
-    rm = await message.reply_text("...")
+    rm = await message.reply_text("CHECKING MY SPEED...")
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
     await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")

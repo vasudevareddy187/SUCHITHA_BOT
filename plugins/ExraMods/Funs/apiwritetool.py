91bf436cc239ab0367deb899f43ed079de6205e9
LATHEESH REDDY <100457240+Alatheesh@users.noreply.github.com>
2023-09-03 06:53:41 +0530

    Update apiwritetool.py
    Signed-off-by: LATHEESH REDDY <100457240+Alatheesh@users.noreply.github.com>

--- plugins/ExraMods/Funs/apiwritetool.py
+++ plugins/ExraMods/Funs/apiwritetool.py
@@ -1,20 +1,20 @@
- from pyrogram import Client, filters
- from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message   
- import requests 
- @Client.on_message(filters.command("write")) 
- async def handwrite(_, message: Message): 
-     if message.reply_to_message: 
-         text = message.reply_to_message.text 
-     else: 
-         text =message.text.split(None, 1)[1] 
-     m =await message.reply_text( "`Please wait...,\n\nWriting your text...`") 
-     write = requests.get(f"https://apis.xditya.me/write?text={text}").url 
-  
-     caption = f""" 
- sᴜᴄᴇssғᴜʟʟʏ ᴡʀɪᴛᴛᴇɴ ᴛᴇxᴛ 💘
- 
- 🥀 **ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ :** {message.from_user.mention} 
- """ 
-     await m.delete() 
-     await message.reply_photo(photo=write,caption=caption) 
- 
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+import requests
+
+@Client.on_message(filters.command("write"))
+async def handwrite(_, message: Message):
+    if message.reply_to_message:
+        text = message.reply_to_message.text
+    else:
+        text = message.text.split(None, 1)[1]
+    m = await message.reply_text("`Please wait...\n\nWriting your text...`")
+    write = requests.get(f"https://apis.xditya.me/write?text={text}").url
+
+    caption = f"""
+    sᴜᴄᴇssғᴜʟʟʏ ᴡʀɪᴛᴛᴇɴ ᴛᴇxᴛ 💘
+    
+    🥀 **ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ :** {message.from_user.mention}
+    """
+    await m.delete()
+    await message.reply_photo(photo=write, caption=caption)

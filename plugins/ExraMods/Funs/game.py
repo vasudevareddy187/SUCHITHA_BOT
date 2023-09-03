3b796780274b15f9b543eef0bc0d19e5907b48a6
LATHEESH REDDY <100457240+Alatheesh@users.noreply.github.com>
2023-09-03 21:27:50 +0530

    Create game.py
    Signed-off-by: LATHEESH REDDY <100457240+Alatheesh@users.noreply.github.com>

---
+++ plugins/ExraMods/Funs/game.py
@@ -0,0 +1,17 @@
+from pyrogram import Client,filters
+from pyrogram.types import Message
+
+
+@Client.on_message(filters.command("dice"))
+async def throw_dice(client, message: Message):
+    six = False
+
+    c = message.chat.id
+    if not six:
+        return await client.send_dice(c, "🎲")
+
+    m = await client.send_dice(c, "🎲")
+
+    while m.dice.value != 6:
+        await m.delete()
+        m = await client.send_dice(c, "🎲")

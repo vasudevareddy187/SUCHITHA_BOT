99b86f10219c70612c331bb1de49c159a5564ad1
LATHEESH REDDY <100457240+Alatheesh@users.noreply.github.com>
2023-09-05 07:47:13 +0530

    Update broadcast.py
    Signed-off-by: LATHEESH REDDY <100457240+Alatheesh@users.noreply.github.com>

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -7,30 +7,31 @@ import time
 from database.users_chats_db import db
 from info import ADMINS
 import asyncio
-        
+
 @Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
 # THOSE WHO MADE FUN OF THE VAZHA TREE##fi
 async def verupikkals(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
-    sts = await message.reply_text(
-        text='Broadcasting your messages...'
-    )
+    sts = await message.reply_text(text='Broadcasting your messages...')
     start_time = time.time()
     total_users = await db.total_users_count()
     done = 0
     blocked = 0
     deleted = 0
-    failed =0
-
+    failed = 0
     success = 0
+    
     async for user in users:
-        pti, sh = await broadcast_messages(int(user['id']), b_msg)
+        first_name = user.get('first_name', '')
+        last_name = user.get('last_name', '')
+        formatted_msg = b_msg.text.replace('{first_name}', first_name).replace('{last_name}', last_name)
+        pti, sh = await broadcast_messages(int(user['id']), Message(text=formatted_msg))
         if pti:
             success += 1
         elif pti == False:
             if sh == "Blocked":
-                blocked+=1
+                blocked += 1
             elif sh == "Deleted":
                 deleted += 1
             elif sh == "Error":
@@ -38,8 +39,9 @@ async def verupikkals(bot, message):
         done += 1
         await asyncio.sleep(2)
         if not done % 20:
-            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
-    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
+            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
+    
+    time_taken = datetime.timedelta(seconds=int(time.time() - start_time))
     await sts.delete()
     await bot.send_message(message.chat.id, f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
 
